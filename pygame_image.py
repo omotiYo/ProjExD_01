fbda74e15a698df3b431287ab367baea28c11a61
Rin <c0a2205288@edu.teu.ac.jp>
2023-06-13 17:31:38 +0900

    本番１

--- pygame_image.py
+++ pygame_image.py
@@ -8,7 +8,8 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     kk_img = pg.transform.flip(pg.image.load("ex01/fig/3.png"), True, False)
-    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 5, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0), 
+               pg.transform.rotozoom(kk_img, 5, 1.0), pg.transform.rotozoom(kk_img, 0, 1.0)]
     tmr = 0
     x = 0
 
@@ -20,10 +21,10 @@ def main():
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img, [1600-x, 0])
         
-        screen.blit(kk_imgs[tmr%2], [300, 200])
+        screen.blit(kk_imgs[tmr%5], [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(100)
+        clock.tick(500)
 
 
 if __name__ == "__main__":
